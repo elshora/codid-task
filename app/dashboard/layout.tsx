8745a9fcef50cce2ac0af05e3884910c017c3856
Mahmoud ELshora <67161371+elshora@users.noreply.github.com>
2023-11-21 07:48:12 +0200

    fix deploy issue

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -5,7 +5,7 @@ const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full relative">
       <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-80 bg-gray-900">
-        <Sidebar isPro={true} apiLimitCount={1} />
+        <Sidebar />
       </div>
       <main className="md:pl-72 pb-10">
         <Navbar />
